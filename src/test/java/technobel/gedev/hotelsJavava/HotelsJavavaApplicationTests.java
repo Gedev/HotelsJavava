990c2412196cd417514157b789fb1fad4002acd8
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-10-14 10:42:44 +0200

    first commit

---
+++ src/test/java/technobel/gedev/hotelsJavava/HotelsJavavaApplicationTests.java
@@ -0,0 +1,13 @@
+package technobel.gedev.hotelsJavava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsJavavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
